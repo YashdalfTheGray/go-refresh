dbf1eebaeed4bc8d35be0cf04123eef12204c901
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-10 10:23:51 -0700

    okay, created a helper but probably is a better way to do it

--- trees.go
+++ trees.go
@@ -5,11 +5,16 @@ import "golang.org/x/tour/tree"
 // Walk walks the tree t sending all values from
 // the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
+	walkHelper(t, ch)
+	close(ch)
+}
+
+func walkHelper(t *tree.Tree, ch chan int) {
 	if t.Left != nil {
-		Walk(t.Left, ch)
+		walkHelper(t.Left, ch)
 	}
 	ch <- t.Value
 	if t.Right != nil {
-		Walk(t.Right, ch)
+		walkHelper(t.Right, ch)
 	}
 }
