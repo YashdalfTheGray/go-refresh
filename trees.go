5772942a72c0f9bac0e2b596d496b1d6a7dd949b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-11 09:55:18 -0700

    implemented same using walk and goroutines

--- trees.go
+++ trees.go
@@ -18,3 +18,20 @@ func walkHelper(t *tree.Tree, ch chan int) {
 		walkHelper(t.Right, ch)
 	}
 }
+
+// Same determines whether the trees t1 and t2
+// contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := range ch1 {
+		if i != <-ch2 {
+			return false
+		}
+	}
+
+	return true
+}
